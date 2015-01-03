ab47bb6882b242db32ef1c5774b552c5eedcd11a
Katsunori SUZUI <kt@7kry.net>
2015-01-03 19:00:31 +0900

    Add setup.py & Rename

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='twsh',
+      version='0.1',
+      description='Twitter Shell for Python',
+      author='7kry',
+      author_email='kt@7kry.net',
+      url='https://github.com/7kry/twsh',
+      packages=['tweetshell'],
+      scripts=['twsh'],
+      requires=['yaml', 'dateutil.tz'],
+     )
